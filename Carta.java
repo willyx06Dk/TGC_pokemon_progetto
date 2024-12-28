466e3619f5c02cec6e7509373c41680cb072dfcb
william19maran <william19.maran@gmail.com>
2024-12-28 12:39:36 +0100

    ampliamento dei metodi nella classe player e creazione della classe Carta

---
+++ Carta.java
@@ -0,0 +1,11 @@
+class Carta{
+    private String nome;
+    private String img;
+    private String categoria;
+
+    public Carta(Strng n, String i, String c){
+        this.nome=n;
+        this.img=i;
+        this.categoria=c;
+    }
+}
--- Player.java
+++ Player.java
@@ -1,13 +1,44 @@
+import java.util.ArrayList;
 class Player{
     private String nome;
     private boolean energia;
-    private Carte[] mano;
+    private ArrayList<Carta> mano;
+    private ArrayList<Carta> panchina;
     private Carta attivo;
+    private ArrayList<Carta> scartate;
 
     public Player(){
         this.nome="";
         this.energia=false;
-        this.mano=new Carte[7];
+        this.mano=new ArrayList<Carta>();
+        this.panchina=new ArrayList<Carta>();
         this.attivo=null;
+        this.scartate=new ArrayList<Carta>();
+    }
+
+    public void setNome(String n){
+        this.nome=n;
+    }
+
+    public void pesca(Carta c){
+        this.mano.add(c);
+    }
+
+    public int cerca(Carta c){
+        for(int i=0; i<this.panchina.lenght; ){
+            if(this.panchina.get(i).corrisponde(c)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void scambia(Carta a, Carta p){
+        int i=this.cerca(p);
+        if(i!=-1){
+            Carta temp=this.panchina.get(i);
+            this.panchina.get(i).assegna(attivo);
+            this.attivo.assegna(temp);
+        }
     }
 }
