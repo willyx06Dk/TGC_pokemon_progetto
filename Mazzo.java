1f4a85f46883de2b6c3372fc1075e48f8e755dad
william19maran <william19.maran@gmail.com>
2024-12-31 12:45:11 +0100

    modifica classe mazzo

--- Mazzo.java
+++ Mazzo.java
@@ -1,6 +1,20 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 class Mazzo {
     private String tipo;
-    private ArrayList<Carta> scartate;
+    private ArrayList<Carta> carte;
+
+    public Mazzo(String t){
+        this.tipo=t;
+        this.carte=new ArrayList<Carta>();
+    }
+
+    public Carta random(){
+        Random r=new Random();
+        int index=r.nextInt(this.carte.size());
+        Carta c=this.carte.get(index);
+        this.carte.remove(c);
+        return c;
+    }
 }
